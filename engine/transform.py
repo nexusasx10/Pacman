c3b01bb307efea2a2949e5ff443b8ea96250af5c
nexusasx10 <andrey.shellex.next@gmail.com>
2024-03-02 03:35:08 +0400

    Merge engine and library transforms

--- engine/transform.py
+++ engine/transform.py
@@ -1,19 +1,35 @@
+from math import cos, sin
+
 from engine.components import Component
-from engine.geometry import Matrix3x3
+from library.geometry import Vector
+from library.utils import not_none_or_default
 
 
-class Transform2d(Component):
+class Transform(Component):
 
-    def __init__(self, owner):
+    def __init__(self, owner, parent=None, position=None, rotation=None):
         super().__init__(owner)
 
-        self._matrix = Matrix3x3.identity()
+        self.position = not_none_or_default(position, Vector.zero())
+        self.rotation = not_none_or_default(rotation, 0)
+        self.parent = parent
+        self.children = []
 
-        self.parent = None
-        self._children = []
-
-    def translate(self, x, y):
-        pass
+    def move(self, offset):
+        self.position += offset
+        for child in self.children:
+            child.move(offset)
 
     def rotate(self, angle):
-        pass
+        self.rotation += angle
+        for child in self.children:
+            child.position.x = (child.position.x - self.position.x) * cos(angle) - (child.position.y - self.position.y) * sin(angle) + self.position.x
+            child.position.y = (child.position.x - self.position.x) * sin(angle) + (child.position.y - self.position.y) * cos(angle) + self.position.y
+
+    def add_child(self, child):
+        self.children.append(child)
+        child.parent = self
+
+    def remove_child(self, child):
+        self.children.remove(child)
+        child.parent = None
